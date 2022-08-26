b25824501986dc8b0951501a43c93a60c1621a64
Lil Ferry <shawney134@gmail.com>
2022-08-25 21:51:50 -0400

    fixed path errors

--- server.js
+++ server.js
@@ -14,42 +14,42 @@ app.use(express.static("public")); // for serving static files
 
 // api routing 
 app.get('/api/notes', (req, res) => { 
-  readFile('./db/db.json', 'utf-8').then((data) => {
+  readFile('C:/Users/shawn/notetaker/Develop/db/db.json', 'utf-8').then((data) => {
     notes = [].concat(JSON.parse(data));
     res.json(notes);
   })});
 
   app.post('/api/notes', (req, res) => {  
     const note = req.body; 
-    readFile('./db/db.json', 'utf-8').then((data) => { 
+    readFile('C:/Users/shawn/notetaker/Develop/db/db.json', 'utf-8').then((data) => { 
     const notes = [].concat(JSON.parse(data)); 
     note.id = uuidv4();
     notes.push(note);
     return notes;
     }).then((notes) => {
-      writeFile('./db/db.json', JSON.stringify(notes));
+      writeFile('C:/Users/shawn/notetaker/Develop/db/db.json', JSON.stringify(notes));
       res.json(notes);
     })
   });
 
 app.delete('/api/notes/:id', (req, res) =>{ //delete note
-const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json"))); //read db.json file
+const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "C:/Users/shawn/notetaker/Develop/db/db.json"))); //read db.json file
 const delNote = notes.filter(rmvNote => rmvNote.id !== req.params.NoteId); //filter out the note to be deleted
-fs.writeFileSync("./db/db.json", JSON.stringify(delNote)); //write to db.json file
+fs.writeFileSync("C:/Users/shawn/notetaker/Develop/db/db.json", JSON.stringify(delNote)); //write to db.json file
 res.json(delNote); //send back the updated notes array
 })
 
 // routing for the index.html files
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile('C:/Users/shawn/notetaker/Develop/public/index.html');
   })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile('C:/Users/shawn/notetaker/Develop/public/notes.html');
   })
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile('C:/Users/shawn/notetaker/Develop/public/notes.html');
   })
 
 app.listen(port, () => {
