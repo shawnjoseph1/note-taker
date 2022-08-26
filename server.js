53112e666c654ae55325a1c769c7b7ae0c1f7cc4
Lil Ferry <shawney134@gmail.com>
2022-08-25 20:46:20 -0400

    fixing 404 and MIME error

--- server.js
+++ server.js
@@ -1,58 +1,57 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const notes = require("./Develop/db/db.json");
-
-
 const app = express();
-const PORT = process.env.PORT || 5500;
+const port = process.env.PORT || 3001;
+// const { v4: uuidv4 } = require('uuid');
+const { readFile } = require('fs/promises');
+const { writeFile } = require('fs/promises');
 
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.json()); // for parsing application/json
 app.use(express.static("public")); // for serving static files
 
-app.get("/api/notes", (req, res) => { //get all notes
-  let results = notes; 
-  res.json(results)
-});
+// api routing 
+app.get('/api/notes', (req, res) => { 
+  readFile('./db/db.json', 'utf-8').then((data) => {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  })});
 
-function createNewNote(body, notesArray) {
-  console.log(body);
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
+  app.post('/api/notes', (req, res) => {  
+    const note = req.body; 
+    readFile('./db/db.json', 'utf-8').then((data) => { 
+    const notes = [].concat(JSON.parse(data)); 
+    note.id = uuidv4();
+    notes.push(note);
+    return notes;
+    }).then((notes) => {
+      writeFile('./db/db.json', JSON.stringify(notes));
+      res.json(notes);
+    })
+  });
 
-  return note;
-}
-
-app.post("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json")); //get all notes from db.json file
-  const newNotes = req.body; 
-  newNotes.id = notes.length + 1; //set new note id
-  notes.push(newNotes);
-  fs.writeFileSync("./db/db.json", JSON.stringify(notes)); //write new note to db.json
-  res.json(notes)
-});
-
-app.delete('/api/notes/:NoteId', (req, res) =>{ //delete note
+app.delete('/api/notes/:id', (req, res) =>{ //delete note
 const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json"))); //read db.json file
 const delNote = notes.filter(rmvNote => rmvNote.id !== req.params.NoteId); //filter out the note to be deleted
 fs.writeFileSync("./db/db.json", JSON.stringify(delNote)); //write to db.json file
 res.json(delNote); //send back the updated notes array
 })
 
-// server statistics
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+// routing for the index.html files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+  })
 
-app.listen(PORT, function () {
-  console.log("App is now on PORT" + PORT);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+  })
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+  })
+
+app.listen(port, () => {
+  console.log(`App is now on PORT ${port}`);
 });
