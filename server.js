afc00273411b9bde35661fb4fa983f9230f4388e
Lil Ferry <shawney134@gmail.com>
2022-08-26 17:41:44 -0400

    fixed issue with css and 404 error, new issue with not being able to save or grab notes

--- server.js
+++ server.js
@@ -2,55 +2,68 @@
 const fs = require("fs"); 
 const path = require("path");
 const express = require("express");
+const { notes } = require("./Develop/db/db.json");
+
+
 const app = express();
 const port = process.env.PORT || 3001;
-const { readFile } = require('fs/promises');
-const { writeFile } = require('fs/promises');
-
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.json()); // for parsing application/json
-app.use(express.static("public")); // for serving static files
+app.use(express.static("./Develop/public")); 
 
 // api routing 
-app.get('/api/notes', (req, res) => { 
-  readFile('C:/Users/shawn/notetaker/Develop/db/db.json', 'utf-8').then((data) => {
-    notes = [].concat(JSON.parse(data));
-    res.json(notes);
-  })});
+function createNewNote(body, notesArray) {
+  console.log(body);
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
 
-  app.post('/api/notes', (req, res) => {  
+  return note;
+}
+
+app.get("/api/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+  app.post("/api/notes", (req, res) => {  
     const note = req.body; 
-    readFile('C:/Users/shawn/notetaker/Develop/db/db.json', 'utf-8').then((data) => { 
+    readFile("./Develop/db/db.json", 'utf-8').then((data) => { 
     const notes = [].concat(JSON.parse(data)); 
+    note.id = notes.length + 1;
     notes.push(note);
     return notes;
     }).then((notes) => {
-      writeFile('C:/Users/shawn/notetaker/Develop/db/db.json', JSON.stringify(notes));
+      writeFile("./Develop/db/db.json", JSON.stringify(notes));
       res.json(notes);
     })
   });
 
 app.delete('/api/notes/:id', (req, res) =>{ //delete note
-const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "C:/Users/shawn/notetaker/Develop/db/db.json"))); //read db.json file
+const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./Develop/db/db.json"))); //read db.json file
 const delNote = notes.filter(rmvNote => rmvNote.id !== req.params.NoteId); //filter out the note to be deleted
-fs.writeFileSync("C:/Users/shawn/notetaker/Develop/db/db.json", JSON.stringify(delNote)); //write to db.json file
+fs.writeFileSync("./Develop/db/db.json", JSON.stringify(delNote)); //write to db.json file
 res.json(delNote); //send back the updated notes array
 })
 
-// routing for the index.html files
-app.get('/', (req, res) => {
-  res.sendFile('C:/Users/shawn/notetaker/Develop/public/index.html');
-  })
+// routing for the html files
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
 
-app.get('*', (req, res) => {
-  res.sendFile('C:/Users/shawn/notetaker/Develop/public/notes.html');
-  })
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
-app.get('/notes', (req, res) => {
-  res.sendFile('C:/Users/shawn/notetaker/Develop/public/notes.html');
-  })
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
-app.listen(port, () => {
-  console.log(`App is now on PORT ${port}`);
+
+app.listen(port, function() { 
+  console.log("App is now on PORT" + port);
 });
