73f398b974abefefd0fb50607ad5a209097b2284
Lil Ferry <shawney134@gmail.com>
2022-08-26 19:08:19 -0400

    working delete button, now able to view saved notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const fs = require("fs"); 
 const path = require("path");
 const express = require("express");
-const { notes } = require("./Develop/db/db.json");
+
 
 
 const app = express();
@@ -12,19 +12,6 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.use(express.json()); // for parsing application/json
 app.use(express.static("./Develop/public")); 
 
-
-function createNewNote(body, notesArray) { // create a new note
-  console.log(body); // log the body of the note to the console
-  const note = body; // create a new note object
-  notesArray.push(note); // push the note to the notes array
-  fs.writeFileSync( // write the new note to the notes.json file
-    path.join(__dirname, "./db/db.json"), // join the path to the db.json file
-    JSON.stringify({ notes: notesArray }, null, 2) // stringify the notes array and indent it by 2 spaces
-  );
-
-  return note; // return the note
-}
-
 app.get("/api/notes", (req, res) => { // get the notes from the notes.json file
   res.sendFile(path.join(__dirname, "./Develop/db/db.json")) // send the notes.json file
 }); 
@@ -33,17 +20,21 @@ app.get("/api/notes", (req, res) => { // get the notes from the notes.json file
 app.post("/api/notes", (req, res) => {
   const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
   const newNotes = req.body;
+  req.body.id = notes.length.toString();
   notes.push(newNotes);
   fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes))
   res.json(notes);
 });
 
+// // delete route for the notes.json file and deletes the note with the matching id from the notes.json file 
+
 app.delete("/api/notes/:id", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-  const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-  fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
-  res.json(delNote);
-})
+  const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
+  const id = req.params.id;
+  const newNotes = notes.filter(note => note.id !== id);
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(newNotes));
+  res.json(newNotes);
+} );
 
 // routing for the html files
 app.get("/notes", (req, res) => {
